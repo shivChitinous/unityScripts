26978703431d53c3ccec5644847fe7c0d25c51ee
Shivam Chitnis <shivamchitnis66@gmail.com>
2024-11-07 09:16:11 -0500

    Revert "Update talk2NiDaqMx.cs"
    This reverts commit a55268308b6d8454329ee8c44e6e53e271dda72e.

--- talk2NiDaqMx.cs
+++ talk2NiDaqMx.cs
@@ -1,141 +1,155 @@
+﻿// Send signal using Janelia.NiDaqMx and a National Instruments (NI) DAQ device with the NI-DAQmx library.
+
 using System;
-using System.Collections;
-using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Janelia
+public class talk2NiDaqMx : MonoBehaviour
 {
-    public class talk2NiDaqMx : MonoBehaviour
-    {
-        public bool showEachWrite = false;
+    public bool showEachWrite = false;
 
-        [Serializable]
-        private class photoDiodeLogEntry : Logger.Entry
+    [Serializable]
+    private class photoDiodeLogEntry : Janelia.Logger.Entry
+    {
+        public double tracePD = 0.0;
+        public double imgFrameTrigger;
+    };
+
+    private photoDiodeLogEntry _currentLogEntry = new photoDiodeLogEntry();
+
+    private void Start()
+    {
+        // Create parameters that are the default values except the specified value.
+        _inputParams = new Janelia.NiDaqMx.InputParams
         {
-            public double tracePD = 0.0;
-            public double imgFrameTrigger;
+            ChannelNames = new string[] { "ai0", "ai1" }
+        };
+
+        // Create parameters that are the default values except the specified values.
+        _outputParams = new Janelia.NiDaqMx.OutputParams()
+        {
+            ChannelNames = new string[] { "ao0" },
+            VoltageMin = -5,
+            VoltageMax = 5
+        };
+
+
+        // To create parameters that are all the default values, use the following:
+        // _outputParams = new Janelia.NiDaqMx.OutputParams();
+
+
+        if (!Janelia.NiDaqMx.CreateInputs(_inputParams))
+        {
+            Debug.Log("Creating input 0 failed");
+            Debug.Log(Janelia.NiDaqMx.GetLatestError());
+            return;
         }
 
-        private photoDiodeLogEntry _currentLogEntry = new photoDiodeLogEntry();
-
-        private void Start()
+        if (!Janelia.NiDaqMx.CreateOutputs(_outputParams))
         {
-            // Create input and output parameters
-            _inputParams = new NiDaqMx.InputParams
-            {
-                ChannelNames = new string[] { "ai0", "ai1" }
-            };
+            Debug.Log("Creating output failed");
+            Debug.Log(Janelia.NiDaqMx.GetLatestError());
+            return;
+        }
 
-            _outputParams = new NiDaqMx.OutputParams()
-            {
-                ChannelNames = new string[] { "ao0" },
-                VoltageMin = -5,
-                VoltageMax = 5
-            };
+        _readData = new double[_inputParams.SampleBufferSize];
 
-            // Initialize inputs and outputs
-            if (!NiDaqMx.CreateInputs(_inputParams))
+
+        // initiate trigger to microscope
+        int numWritten = 0;
+
+        int expectedNumWritten = 1;
+        double writeValue = _outputParams.VoltageMax;
+        //writeValue = 0.1 * writeValue;
+        if (!Janelia.NiDaqMx.WriteToOutputs(_outputParams, writeValue, ref numWritten))
+
+        {
+            Debug.Log("Frame " + Time.frameCount + ": write to output failed");
+            Debug.Log(Janelia.NiDaqMx.GetLatestError());
+        }
+        if (numWritten != expectedNumWritten)
+        {
+            Debug.Log("Frame " + Time.frameCount + ": unexpectedly, wrote " + numWritten + " values");
+        }
+        else if (showEachWrite)
+        {
+            Debug.Log("Frame " + Time.frameCount + ": wrote " + numWritten + " value(s): " + writeValue);
+        }
+
+
+    }
+
+    private void Update()
+    {
+
+        // Reading
+        int numReadPerChannel = 0;
+        if (!Janelia.NiDaqMx.ReadFromInputs(_inputParams, ref _readData, ref numReadPerChannel))
+        {
+            Debug.Log("Frame " + Time.frameCount + ": read from input failed");
+            Debug.Log(Janelia.NiDaqMx.GetLatestError());
+        }
+        else
+        {
+            if (numReadPerChannel > 0)
             {
-                Debug.LogError("Creating input 0 failed");
-                Debug.LogError(NiDaqMx.GetLatestError());
-                return;
+                // log read values
+                for (int i = 0; i < numReadPerChannel; i++)
+                {
+                    //channel 1 (ai0)
+                    int j = Janelia.NiDaqMx.IndexInReadBuffer(0, numReadPerChannel, i);
+                    _currentLogEntry.tracePD = _readData[j];
+
+                    //channel 2 (ai1)
+                    int k = Janelia.NiDaqMx.IndexInReadBuffer(1, numReadPerChannel, i);
+                    _currentLogEntry.imgFrameTrigger = _readData[k];
+
+                    Janelia.Logger.Log(_currentLogEntry);
+
+                }
             }
-
-            if (!NiDaqMx.CreateOutputs(_outputParams))
+            else
             {
-                Debug.LogError("Creating output failed");
-                Debug.LogError(NiDaqMx.GetLatestError());
-                return;
+                Debug.Log("Frame " + Time.frameCount + ": unexpectedly, read " + numReadPerChannel + " values");
             }
+        }
 
-            _readData = new double[_inputParams.SampleBufferSize];
 
-            // Initial trigger to microscope
+        // Writing
+
+        if (Input.anyKeyDown)
+        {
             int numWritten = 0;
-            double writeValue = _outputParams.VoltageMax;
-            if (!NiDaqMx.WriteToOutputs(_outputParams, writeValue, ref numWritten))
+
+            int expectedNumWritten = 1;
+            double writeValue = (_iWrite % 2 == 0) ? _outputParams.VoltageMax : _outputParams.VoltageMin;
+            //writeValue = 0.1 * writeValue;
+            if (!Janelia.NiDaqMx.WriteToOutputs(_outputParams, writeValue, ref numWritten))
+
             {
-                Debug.LogError("Initial write to output failed");
-                Debug.LogError(NiDaqMx.GetLatestError());
+                Debug.Log("Frame " + Time.frameCount + ": write to output failed");
+                Debug.Log(Janelia.NiDaqMx.GetLatestError());
+            }
+            if (numWritten != expectedNumWritten)
+            {
+                Debug.Log("Frame " + Time.frameCount + ": unexpectedly, wrote " + numWritten + " values");
             }
             else if (showEachWrite)
             {
                 Debug.Log("Frame " + Time.frameCount + ": wrote " + numWritten + " value(s): " + writeValue);
             }
-
-            // Start coroutine to read data from inputs
-            StartCoroutine(ReadDataCoroutine());
+            _iWrite++;
         }
 
-        private void Update()
-        {
-            // Writing when any key is pressed
-            if (Input.anyKeyDown)
-            {
-                int numWritten = 0;
-                double writeValue = (_iWrite % 2 == 0) ? _outputParams.VoltageMax : _outputParams.VoltageMin;
-
-                if (!NiDaqMx.WriteToOutputs(_outputParams, writeValue, ref numWritten))
-                {
-                    Debug.LogError("Write to output failed on key press");
-                    Debug.LogError(NiDaqMx.GetLatestError());
-                }
-                else if (showEachWrite)
-                {
-                    Debug.Log("Frame " + Time.frameCount + ": wrote " + numWritten + " value(s): " + writeValue);
-                }
-
-                _iWrite++;
-            }
-        }
-
-        private IEnumerator ReadDataCoroutine()
-        {
-            while (true)
-            {
-                int numReadPerChannel = 0;
-                Task<bool> readTask = NiDaqMx.ReadFromInputsAsync(_inputParams, _readData, numReadPerChannel);
-
-                yield return new WaitUntil(() => readTask.IsCompleted);
-
-                if (readTask.Result && numReadPerChannel > 0)
-                {
-                    for (int i = 0; i < numReadPerChannel; i++)
-                    {
-                        // Log data for channel ai0
-                        int j = NiDaqMx.IndexInReadBuffer(0, numReadPerChannel, i);
-                        _currentLogEntry.tracePD = _readData[j];
-
-                        // Log data for channel ai1
-                        int k = NiDaqMx.IndexInReadBuffer(1, numReadPerChannel, i);
-                        _currentLogEntry.imgFrameTrigger = _readData[k];
-
-                        Logger.Log(_currentLogEntry);
-                    }
-                }
-                else
-                {
-                    Debug.LogWarning("Read failed or returned zero values");
-                    if (!readTask.Result)
-                    {
-                        Debug.LogError(NiDaqMx.GetLatestError());
-                    }
-                }
-
-                // Small delay to prevent too frequent polling
-                yield return new WaitForSeconds(0.05f); // Adjust delay as needed based on your requirements
-            }
-        }
-
-        private void OnDestroy()
-        {
-            NiDaqMx.OnDestroy();
-        }
-
-        private NiDaqMx.InputParams _inputParams;
-        private double[] _readData;
-
-        private NiDaqMx.OutputParams _outputParams;
-        private int _iWrite = 0;
     }
+
+    private void OnDestroy()
+    {
+        Janelia.NiDaqMx.OnDestroy();
+    }
+
+    private Janelia.NiDaqMx.InputParams _inputParams;
+    double[] _readData;
+
+    private Janelia.NiDaqMx.OutputParams _outputParams;
+    int _iWrite = 0;
 }
