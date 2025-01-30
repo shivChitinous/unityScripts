3900d7ac6aef9e35a3f5d16bc14d3431c02ac90f
Shivam Chitnis <shivamchitnis66@gmail.com>
2025-01-29 22:10:16 -0500

    For some reason tiling seems sensitive to whether you load a material or not

--- AnimateCylinderTexture.cs
+++ AnimateCylinderTexture.cs
@@ -36,7 +36,7 @@ public class AnimateCylinderTexture : MonoBehaviour
 
 
     void Start()
-    {   /*
+    {
         cylinderMaterial = Resources.Load(Janelia.CylinderBackgroundResources.MaterialName, typeof(Material)) as Material;
         //Resources.Load(Janelia.CylinderBackgroundResources.MaterialName, typeof(Material)) as Material
 
@@ -44,7 +44,6 @@ public class AnimateCylinderTexture : MonoBehaviour
         {
             Debug.LogError("Could not load material'" + Janelia.CylinderBackgroundResources.MaterialName + "'");
         }
-        */ 
 
         //reset texture based on offset
         elevation = offsetEl;
@@ -53,15 +52,12 @@ public class AnimateCylinderTexture : MonoBehaviour
 
         Vector2 offset = new Vector2(x, y);
 
-        //cylinderMaterial.SetTextureOffset("_MainTex", offset);
+        cylinderMaterial.SetTextureOffset("_MainTex", offset);
 
         //log values
         _currentLogEntry.xpos = x;
         _currentLogEntry.ypos = y;
         Janelia.Logger.Log(_currentLogEntry);
-
-        int which = 0;
-        Janelia.BackgroundUtilities.SetCylinderTextureOffset(offset, which);
     }
 
     void Update()
@@ -78,7 +74,7 @@ public class AnimateCylinderTexture : MonoBehaviour
             }
         }
 
-        if (Time.time >= (waitTime+delaySeconds))
+        if (cylinderMaterial & Time.time >= (waitTime+delaySeconds))
         {
 
             float dTime = Time.time - (waitTime+delaySeconds);
@@ -110,11 +106,8 @@ public class AnimateCylinderTexture : MonoBehaviour
             //stop if done
             if (currentStep*vel <= (repeats * 2 * numElevationSteps * sweepRepeatVec[sweepRepeatVec.Length-1] * vRotDeg_per_sec.Length))
             {
-                
-                int which = 0;
-                Janelia.BackgroundUtilities.SetCylinderTextureOffset(offset, which);
 
-                //cylinderMaterial.SetTextureOffset("_MainTex", offset);
+                cylinderMaterial.SetTextureOffset("_MainTex", offset);
 
                 //log values
                 _currentLogEntry.xpos = x;
